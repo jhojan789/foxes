b87ef768203bed55fca1c06d5c7d6acce207c205
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-19 16:58:32 -0500

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import './styles/index.css';
+
+const app = document.getElementById('app');
+
+const h1 = document.createElement('h1');
+h1.textContent = 'Foxes';
+app.append(h1);
+
+
+
+console.log('Hello world');
