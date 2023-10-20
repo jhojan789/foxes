13d867ad8e4c5761270a6754682a071a30db9b58
Jhojan Tobón <jhojantobonm@gmail.com>
2023-10-20 12:42:49 -0500

    Passed html images to js Created button Improved styles

--- src/index.js
+++ src/index.js
@@ -1,10 +1,39 @@
 import './styles/index.css';
 
+{/* <div class="fox-item">
+        <img 
+          class="fox-image"
+          src="https://randomfox.ca/images/2.jpg"
+          alt=""
+          >
+      </div> */}
 
+const foxContainer = document.getElementById('foxes-container');
+const foxAdd = document.getElementById('fox-add');
 
+const min = 1;
+const max = 123;
 
+const foxItem = ()=>{
+  let random = Math.floor(Math.random()* (max - min) + min);
+  const div = document.createElement('div');
+  div.className = 'fox-item';
 
+  const img = document.createElement('img');
+  img.className = "fox-image";
+  img.src = `https://randomfox.ca/images/${random}.jpg`;
+  img.alt = "";
 
+  div.appendChild(img);
 
+  return div;
+}
 
-console.log('Hello world');
+const addFox = () => {
+  const foxImg = foxItem();
+  foxContainer.append(foxImg);
+  
+}
+
+foxAdd.addEventListener('click', addFox);
+
